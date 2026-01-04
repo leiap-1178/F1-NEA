e9af98c92582da21f2fa1a7d4b0a60ce74b18c87
leiap-1178 <leiapitrora@gmail.com>
2026-01-04 19:18:23 +0000

    Update NEA.py

--- NEA.py
+++ NEA.py
@@ -1,48 +1,46 @@
-
 from fastf1.core import Session
 from kivy.config import Config
 from kivy.graphics.context_instructions import Image
 from kivy.uix.boxlayout import BoxLayout
-Config.set('graphics', 'width', '360') #mobile screen with
-Config.set('graphics', 'height', '640') #mobile screen height
+# Imports the needed tools
 
+#imports the libraries plotting features
 from fastf1 import plotting
-
 from matplotlib import pyplot as plt
-
 import fastf1
 from fastf1 import plotting
 
 
-# Enable Matplotlib patches for plotting timedelta values and load
-# FastF1's dark color scheme
+# Enable Matplotlib to plot the timedelta values and load
+# FastF1's dark color scheme for the graph
 fastf1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme='fastf1')
 
-
+#Loads a race session which is set to a default right now
 race: Session = fastf1.get_session(2025, "Azerbaijan", 'R')
 race.load()
 
+#assigns a line style to each driver based on their number
 my_styles = [
-    # style for each first driver
     {'color': 'auto', 'linestyle': 'solid', 'linewidth': 5, 'alpha': 0.3},
-    # style for each second driver
     {'color': 'auto', 'linestyle': 'solid', 'linewidth': 1, 'alpha': 0.7}
 ]
-
+#sets the size of the graph figure
 fig, ax = plt.subplots(figsize=(8, 5))
 
+#for each driver in the list retrieves the data
 for driver in ('HAM', 'PER', 'VER', 'RUS'):
     laps = race.laps.pick_drivers(driver).pick_quicklaps().reset_index()
 
-    # here, we now use ``style=my_style`` to use the custom styling
+    # customise the style of the graph (labels)
     style = plotting.get_driver_style(identifier=driver,
                                       style=my_styles,
                                       session=race)
 
     ax.plot(laps['LapTime'], **style, label=driver)
 
-# add axis labels and a legend
+# add axis labels 
 ax.set_xlabel("Lap Number")
 ax.set_ylabel("Lap Time")
 plotting.add_sorted_driver_legend(ax, race)
-plt.show()
+
+plt.show()#Executes the graph visual 
